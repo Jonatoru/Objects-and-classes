0b9e23b1f918dcfcccd7d0f8ce2cc292539c5f59
Jonatoru <jetrence95@gmail.com>
2022-12-22 00:51:34 +0500

    Решил домашнее задание по Методам объектов.

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,32 @@
+import java.util.Objects;
+
 public class Book {
     private String nameBook;
     private Author author;
     private int publicationYear;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(nameBook, book.nameBook) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, publicationYear);
+    }
+
     public Book(Author author, String nameBook, int publicationYear) {
         this.nameBook = nameBook;
         this.author = new Author (author.getAuthorName(), author.getAuthorSurname());
         this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор, название книги, год издания: " + author + ", "+ nameBook +", " + publicationYear +'.';
     }
 
     public String getNameBook() {
